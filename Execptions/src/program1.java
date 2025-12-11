3b68c49ebbabb2802deaa020ec3ae706ef06540d
Anas <ahmedanas1433@gmail.com>
2025-12-11 10:58:03 +0530

    Second commit

--- Execptions/src/program1.java
+++ Execptions/src/program1.java
@@ -2,8 +2,8 @@ public class program1 {
     static void main(String[] args) {
         try{
 
-        int a=4;
-        int b=2;
+        int a=5;
+        int b=3;
 
             int c=a/b;
             System.out.println(c);
